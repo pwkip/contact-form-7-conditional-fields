f8048e3cd46e75b83453afea6cdad4df0b3c46e1
swimming pool <97299566+web-swpl@users.noreply.github.com>
2023-07-06 10:11:40 +0000

    Fix jQuery vs HTMLElement detection

--- js/scripts_es6.js
+++ js/scripts_es6.js
@@ -51,7 +51,7 @@ let wpcf7cf_change_time_ms = 100; // the timeout after a change in the form is d
 
 if (window.wpcf7 && !wpcf7.setStatus) {
     wpcf7.setStatus = ( form, status ) => {
-        form = form.length ? form[0] : form; // if form is a jQuery object, only grab te html-element
+        form = form instanceof HTMLElement ? form : form[0]; // if form is a jQuery object, only grab te html-element
         const defaultStatuses = new Map( [
             // 0: Status in API response, 1: Status in HTML class
             [ 'init', 'init' ],
